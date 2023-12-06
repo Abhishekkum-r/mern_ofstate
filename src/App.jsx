839573739dc226d1797acff1f3e1ce1cc1fb86dc
Abhishek Kumar Singh <2021021006@mmmut.ac.in>
2023-12-07 04:11:09 +0530

    created pages and routes for webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/home';
+import SignIn from './pages/sign_in';
+import SignUp from './pages/signup';
+import About from './pages/About';
+import Profile from './pages/profile';
 export default function App() {
   return (
-    <h1 className='text-red-700'>App</h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/Home" element={<Home />} />
+      <Route path="/SignIn" element={<SignIn />} />
+      <Route path="/SignUp" element={<SignUp />} />
+      <Route path="/Profile" element={<Profile />} />
+      <Route path="/About" element={<About />} />
+
+    </Routes>
+    
+    </BrowserRouter>
   )
 }
